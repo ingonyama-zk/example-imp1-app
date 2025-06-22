cd2686815f6f2e0eb81eabce83cafbe5ad5781eb
Jeremy Felder <jeremy.felder1@gmail.com>
2025-06-22 20:13:41 +0300

    Init example app

---
+++ IcicleSnarkFrameworkTest/IcicleSnarkFrameworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcicleSnarkFrameworkTestApp.swift
+//  IcicleSnarkFrameworkTest
+//
+//  Created by Jeremy Felder on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct IcicleSnarkFrameworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainMenuView()
+        }
+    }
+}
